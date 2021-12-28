41c60b71e8288ce21b48cab6946a3e76ed1b57b8
lbm364dl <geometrydashm364@gmail.com>
2021-12-28 17:27:14 +0100

    añadido plot

--- expDiscN.py
+++ expDiscN.py
@@ -1,14 +1,16 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import eqs
 
-to_file = False
+to_file = True
 func = eqs.fIkeda
 dfunc = eqs.dfIkeda
 N = 2
 trans = 1000
-its = 30000
+its = 50000
 print_freq = 100
 exps = []
+times = []
 cur_exps = np.zeros(N)
 J = np.eye(N)
 x = np.array([0.5, 0.5])
@@ -24,12 +26,18 @@ for i in range(trans, its+1):
     cur_exps += np.log(np.abs(np.diag(R)))
 
     if i % print_freq == 0:
-        exps.append((i, cur_exps/(i-trans+1)))
+        exps.append(cur_exps/(i-trans+1))
+        times.append(i)
 
     x = func(x)
 
 if to_file:
     with open('liapDiscN.dat', 'w') as f:
-        f.write('\n'.join(' '.join(map(str, [i, *xps])) for i, xps in exps))
+        f.write('\n'.join(' '.join(map(str, [i, *xps])) for i, xps in zip(times, exps)))
 
+exps = list(zip(*exps))
+for i in range(N):
+    plt.plot(times, exps[i])
+
+plt.show()
 print(cur_exps/(its-trans+1))
